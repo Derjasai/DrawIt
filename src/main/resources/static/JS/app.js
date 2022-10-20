99b9063b01ee47f916a6330a6a85f61ce3bbc047
derjasai <elholaquehace88@gmail.com>
2022-10-20 05:12:11 -0500

    Avance con el websocket

--- src/main/resources/static/JS/app.js
+++ src/main/resources/static/JS/app.js
@@ -2,9 +2,11 @@ var apiclient = apiclient;
 var app = (function (){
 
     var name;
-
+    var stompClient = null; 
+    
     function createUser(){
         name = $("#userName").val();
+        connectAndSubscribe();
         apiclient.addUser(name).then(()=>{
             alert("Hola soy una prueba")
         })
@@ -14,6 +16,21 @@ var app = (function (){
     function getUser(data){
         apiclient.getUser("daniel", prueba);
     }
+
+    var connectAndSubscribe = function () {
+        console.info('Connecting to WS...');
+        var socket = new SockJS('/stompendpoint');
+        stompClient = Stomp.over(socket);
+
+        //subscribe to /topic/TOPICXX when connections succeed
+        stompClient.connect({}, function (frame) {
+            console.log('Connected: ' + frame);
+            stompClient.subscribe("/topic/"+name, function (eventbody) {
+                alert(eventbody);
+            });
+        });
+
+    };
 
     var prueba = function (data){
         $("#prueba").text(data.name);
